14facfb2b817e9e712ac00840f1c2d129a66f996
FlorianWiekhorst <florian.wiekhorst@haw-hamburg.de>
2015-12-06 15:17:02 +0100

    Create header.php

---
+++ php/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="de">
+
+	<head>
+		<meta charset="utf-8">
+		<link type="text/css" rel="stylesheet" href="css/stylesheet.css"/>
+		<title><?php echo($title);?></title>
+	</head>
+	
+	<body>
